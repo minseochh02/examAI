2f411212d1892195f44bc1155843a2d5bc266d1a
minseochh02 <minseochh02@gmail.com>
2024-07-30 15:09:44 +0900

    testing auth

--- src/app/api/save-user-data/route.ts
+++ src/app/api/save-user-data/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
 
+
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
 
